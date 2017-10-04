39949ec48b439e9bf8b791a98c525c5364d4ff4a
Ivaylo Terziev <ivayloterziev91@gmail.com>
2017-10-05 00:36:45 +0300

    Add ContactUsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RichFooterComponent } from './footer/rich-footer/rich-footer.component';
+import { ContactUsComponent } from './form/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RichFooterComponent
+    RichFooterComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule

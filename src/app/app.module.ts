21ffcb32292a9e1325b34141bd714ab6d3ff3297
Ivaylo Terziev <ivayloterziev91@gmail.com>
2017-10-04 23:19:38 +0300

    Add RichFooterComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RichFooterComponent } from './footer/rich-footer/rich-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RichFooterComponent
   ],
   imports: [
     BrowserModule

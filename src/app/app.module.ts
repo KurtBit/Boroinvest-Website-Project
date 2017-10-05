c8ed1989db8f85cfb809fe783f95404b009fddd5
Ivaylo Terziev <ivayloterziev91@gmail.com>
2017-10-06 00:15:08 +0300

    Add OfferComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RichFooterComponent } from './footer/rich-footer/rich-footer.component';
 import { ContactUsComponent } from './form/contact-us/contact-us.component';
+import { OfferComponent } from './our-offer/offer/offer.component';
+
+// import { KurtbitFrontAngularModule } from 'kurtbit-front-angular';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     RichFooterComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    OfferComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // KurtbitFrontAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]

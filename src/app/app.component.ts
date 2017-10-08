fbab5ea13558dbd1746e67a0234059e41a2b95ca
Ivaylo Terziev <ivayloterziev91@gmail.com>
2017-10-09 02:01:06 +0300

    Add minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,16 @@ export class AppComponent {
   title = 'app';
 
   carouselItems: { src: string, isActive: boolean }[] = [
-    { src: 'https://www.w3schools.com/bootstrap/la.jpg', isActive: true },
-    { src: 'https://www.w3schools.com/bootstrap/chicago.jpg', isActive: false },
-    { src: 'https://www.w3schools.com/bootstrap/ny.jpg', isActive: false }
+    { src: '../assets/images/Destinations/dobrovnik.jpg', isActive: true },
+    { src: '../assets/images/Destinations/pariss.jpg', isActive: false },
+    { src: '../assets/images/Destinations/malta.jpg', isActive: false },
+    { src: '../assets/images/Destinations/venecia.jpg', isActive: false }
   ]
 
   galleryItems: { src: string, isActive: boolean }[] = [
-    { src: '../../../assets/images/Destinations/venecia.jpg', isActive: true },
-    { src: '../../../assets/images/Destinations/pariss.jpg', isActive: false },
-    { src: '../../../assets/images/Destinations/malta.jpg', isActive: false },
-    { src: '../../../assets/images/Destinations/dobrovnik.jpg', isActive: false }
+    { src: '../assets/images/Destinations/dobrovnik.jpg', isActive: true },
+    { src: '../assets/images/Destinations/pariss.jpg', isActive: false },
+    { src: '../assets/images/Destinations/malta.jpg', isActive: false },
+    { src: '../assets/images/Destinations/venecia.jpg', isActive: false }
   ]
 }

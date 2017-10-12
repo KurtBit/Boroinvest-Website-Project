ebec18b687e2b245ae36827211459d3d15365eb7
Ivaylo Terziev <ivayloterziev91@gmail.com>
2017-10-13 00:21:51 +0300

    Add basic sliding animation to Carousel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'Carousel',
@@ -7,16 +7,16 @@ import { Component, Input } from '@angular/core';
             <div class="carousel-inner">
                 <div class="item-container" *ngFor="let img of carouselItems">
                     <div class="item" *ngIf="img.isActive">
-                        <img [src]="img.src" alt="Los Angeles">
+                        <img #image [src]="img.src" alt="Los Angeles">
                     </div>
                 </div>
             </div>
 
-            <a class="left carousel-control" (click)="handleLeftClick($event)">
+            <a id="left" class="left carousel-control" (click)="handleLeftClick($event)">
                 <span class="glyphicon glyphicon-chevron-left"></span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="right carousel-control" (click)="handleRightClick($event)">
+            <a id="right" class="right carousel-control" (click)="handleRightClick($event)">
                 <span class="glyphicon glyphicon-chevron-right"></span>
                 <span class="sr-only">Next</span>
             </a>
@@ -26,11 +26,15 @@ import { Component, Input } from '@angular/core';
         `
             img {
                 width: 100%;
+                position: relative;
+                transition: 2s;
             }
         `
     ]
 })
 export class CarouselComponent {
+    @ViewChild('image') currentImage;
+
     @Input() carouselItems : { src: string, isActive: boolean }[];
 
     public handleLeftClick($event) {
@@ -41,15 +45,33 @@ export class CarouselComponent {
             prevIndex += this.carouselItems.length;
         }
 
-        this.carouselItems[index].isActive = false;
-        this.carouselItems[prevIndex].isActive = true;
+        this.currentImage.nativeElement.style = 'left: 0';
+        
+        setTimeout(() => {
+            this.currentImage.nativeElement.style = 'left: 100%';
+        }, 100);
+
+        setTimeout(() => {
+            this.carouselItems[index].isActive = false;
+            this.carouselItems[prevIndex].isActive = true;
+        }, 1500);
+        
     }
 
     public handleRightClick($event) {
         let index =  this.carouselItems.findIndex((x) => x.isActive);
         let nextIndex = (index + 1) % this.carouselItems.length;
 
-        this.carouselItems[index].isActive = false;
-        this.carouselItems[nextIndex].isActive = true;
+        this.currentImage.nativeElement.style = 'right: 0';
+        
+        setTimeout(() => {
+            this.currentImage.nativeElement.style = 'right: 100%';
+        }, 100);
+
+        setTimeout(() => {
+            this.carouselItems[index].isActive = false;
+            this.carouselItems[nextIndex].isActive = true;
+        }, 1000);
+        
     }
 }

ba1961ba4c170b1e96a00773b4dce1d8dc72b715
Dominent <petromilpavlov@gmail.com>
2017-10-15 13:30:16 +0300

    Update Login View

--- src/app/Authentication/Login/login.component.ts
+++ src/app/Authentication/Login/login.component.ts
@@ -5,32 +5,34 @@ import { AuthenticationService } from '../Services/authentication.service';
 @Component({
     selector: 'auth-login',
     template: `
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="well">
-                    <div class="auth-login">
-                        <form (ngSubmit)="onSubmit()" #loginForm="ngForm">
-                            <div class="form-group">
-                                <label for="username">Username</label>
-                                <input type="text" id="username" class="form-control"
-                                    [(ngModel)]="model.username" name="username"
-                                />
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" id="password"  class="form-control"
-                                    [(ngModel)]="model.password" name="password"
-                                />
-                            </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <h2>Login</h2>
+                    <hr>
+                    <div class="well">
+                        <div class="auth-login">
+                            <form (ngSubmit)="onSubmit()" #loginForm="ngForm">
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input type="text" id="username" class="form-control"
+                                        [(ngModel)]="model.username" name="username"
+                                    />
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" id="password"  class="form-control"
+                                        [(ngModel)]="model.password" name="password"
+                                    />
+                                </div>
 
-                            <input type="submit" value="Login" class="btn btn-default">
-                        </form>
-                    </div>
-                </div>                
-            </div>            
+                                <input type="submit" value="Login" class="btn btn-default">
+                            </form>
+                        </div>
+                    </div>                
+                </div>            
+            </div>
         </div>
-    </div>
     `
 })
 export class LoginComponent {

f9359a6d4bc16eb23d99b7d1293171fe1bc0bdb8
Dominent <petromilpavlov@gmail.com>
2017-10-30 01:01:09 +0200

    Format List Offer Date

--- src/app/Admin/Offer/offer.component.ts
+++ src/app/Admin/Offer/offer.component.ts
@@ -8,21 +8,36 @@ import { HttpClient } from '@angular/common/http';
         <h2 style="margin-top: 100px;">Покажи Оферти</h2>
         <hr>
 
-        <div>
-            <ul>
-                <li *ngFor="let offer of Offers">
-                    {{offer.name}}
-                </li>
-            </ul>
-        </div>
+        <table id="mytable" class="table table-bordred table-striped">
+        
+            <thead>
+                <th>Име</th>
+                <th>Автор</th>
+                <th>Дата</th>
+                <th>Цена</th>
+                <th>Промени</th>
+                <th>Изтрий</th>
+            </thead>
+            <tbody>
+                <tr *ngFor="let offer of Offers">
+                    <td>
+                        <a href="#">{{offer.name}}</a>
+                    </td>
+                    <td>{{offer.author}}</td>
+                    <td>{{offer.date | date }}</td>
+                    <td>{{offer.price}}</td>
+                    <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+                    <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
+                </tr>
+            </tbody>
+        </table>
     `
 })
 export class OfferComponent implements OnInit {
     public Offers: OfferModel[];
 
     constructor(
-        private http: HttpClient)
-    { }
+        private http: HttpClient) { }
 
     ngOnInit(): void {
         this.http.get('/api/v1/offers')
